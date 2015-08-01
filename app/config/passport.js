0fe11220f05a22debfba0811f0545156611f8ee4
Blake Johnston <johnstonbl01@gmail.com>
2015-08-01 14:13:34 -0500

    Complete Passport config and routes.

---
+++ app/config/passport.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var TwitterStrategy = require('passport-twitter').Strategy;
+var User = require('../models/users');
+var configAuth = require('./auth');
+
+module.exports = function (passport) {
+	passport.serializeUser(function (user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+
+	passport.use(new TwitterStrategy({
+		consumerKey: configAuth.twitterAuth.consumerKey,
+		consumerSecret: configAuth.twitterAuth.consumerSecret,
+		callbackURL: configAuth.twitterAuth.callbackURL
+	},
+	function (token, tokenSecret, profile, done) {
+		process.nextTick(function () {
+			User.findOne({ 'twitter.id': profile.id }, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+
+					newUser.twitter.id = profile.id;
+					newUser.twitter.token = token;
+					newUser.twitter.username = profile.username;
+					newUser.twitter.displayName = profile.displayName;
+
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,56 @@
 'use strict';
 
 var path = process.cwd();
-
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
 
-module.exports = function (app) {
+module.exports = function (app, passport) {
+
+	function isLoggedIn (req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/login');
+		}
+	}
 
 	var clickHandler = new ClickHandler();
 
 	app.route('/')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/index.html');
+		.get(isLoggedIn, function (req, res) {
+			res.sendFile(path + '/public/index.html', {
+				user: req.user
+			});
 		});
+
+	app.route('/login')
+		.get(function (req, res) {
+			res.sendFile(path + '/public/login.html');
+		});
+
+	app.route('/logout')
+		.get(function (req, res) {
+			req.logout();
+			res.redirect('/login');
+		});
+
+	app.route('/profile')
+		.get(isLoggedIn, function (req, res) {
+			res.sendFile(path + '/public/profile.html');
+		});
+
+	app.route('/api/user')
+		.get(isLoggedIn, function (req, res) {
+			res.json(req.user.twitter);
+		});
+
+	app.route('/auth/twitter')
+		.get(passport.authenticate('twitter'));
+
+	app.route('/auth/twitter/callback')
+		.get(passport.authenticate('twitter', {
+			successRedirect: '/',
+			failureRedirect: '/login'
+		}));
 
 	app.route('/api/clicks')
 		.get(clickHandler.getClicks)
